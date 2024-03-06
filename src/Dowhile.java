577a41023cfa70c829163815b7285dcdaefa3c0d
ganesh739632 <panduganeshbanoth@gmail.com>
2024-03-06 16:12:41 -0600

    Initial Do while

---
+++ src/Dowhile.java
@@ -0,0 +1,10 @@
+public class Dowhile {
+    public static void main(String[] args) {
+        int k=6;
+        do{
+            System.out.println("ok");
+            k++;
+        }
+        while(k<=10);
+    }
+}
